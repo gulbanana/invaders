416d8e4a44586e4f7524a0baab24c666830f07dc
Thomas Castiglione <castiglione@gmail.com>
2016-09-14 23:18:03 +0800

    more invaderlike bouncing

--- src/invaders.cr
+++ src/invaders.cr
@@ -1,31 +1,39 @@
 require "./invaders/*"
 
+boss = Sprite.new([Data::MOTHERSHIP])
+
 sprites1 = (1..4).map { Sprite.new([Data::ENEMY_2_DOWN, Data::ENEMY_2_DOWN, Data::ENEMY_2_UP, Data::ENEMY_2_UP]) }
 sprites2 = (1..4).map { Sprite.new([Data::ENEMY_1_DOWN, Data::ENEMY_1_UP]) } 
 sprites3 = (1..4).map { Sprite.new([Data::ENEMY_1_DOWN, Data::ENEMY_1_UP]) }
 
-x_offset = 0
+x_base = 0
+x_off = 1
 while true
   sleep 0.4
   Ansi.clear()
 
-  x = x_offset % 4
+  boss.draw(x_base, 0)
+
+  x = x_base
   sprites1.each do |s|
-    s.draw(x, 1)
+    s.draw(x, 7)
     x += 1
   end
 
-  x = x_offset % 4
+  x = x_base
   sprites2.each do |s|
-    s.draw(x, 8)
+    s.draw(x, 14)
     x += 1
   end
 
-  x =  x_offset % 4
+  x = x_base
   sprites3.each do |s|
-    s.draw(x, 15)
+    s.draw(x, 21)
     x += 1
   end
 
-  x_offset += 1
+  x_base += x_off
+  if (x_base > 2 || x_base < 1)
+    x_off = x_off * -1
+  end
 end
