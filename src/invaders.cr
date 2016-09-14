50392f4d8fd5ecc7e502d99bae9a0cf40604cfc2
Thomas Castiglione <castiglione@gmail.com>
2016-09-14 23:12:03 +0800

    draw a lot more enemies!

--- src/invaders.cr
+++ src/invaders.cr
@@ -1,12 +1,31 @@
 require "./invaders/*"
 
+sprites1 = (1..4).map { Sprite.new([Data::ENEMY_2_DOWN, Data::ENEMY_2_DOWN, Data::ENEMY_2_UP, Data::ENEMY_2_UP]) }
+sprites2 = (1..4).map { Sprite.new([Data::ENEMY_1_DOWN, Data::ENEMY_1_UP]) } 
+sprites3 = (1..4).map { Sprite.new([Data::ENEMY_1_DOWN, Data::ENEMY_1_UP]) }
+
+x_offset = 0
 while true
-  sleep 0.2
-  puts Ansi::CLEAR
-  puts Game::ENEMY_DOWN
+  sleep 0.4
+  Ansi.clear()
 
-  sleep 0.2
-  puts Ansi::CLEAR
-  puts Game::ENEMY_UP 
+  x = x_offset % 4
+  sprites1.each do |s|
+    s.draw(x, 1)
+    x += 1
+  end
+
+  x = x_offset % 4
+  sprites2.each do |s|
+    s.draw(x, 8)
+    x += 1
+  end
+
+  x =  x_offset % 4
+  sprites3.each do |s|
+    s.draw(x, 15)
+    x += 1
+  end
+
+  x_offset += 1
 end
-
--- src/invaders/ansi.cr
+++ src/invaders/ansi.cr
@@ -1,3 +1,10 @@
 module Ansi
-  CLEAR = "\33[2J"
+  def self.clear()
+    print "\33[2J"
+  end
+
+  # tput cup #{l} #{c} = \E[%i%p1%d;%p2%dH
+  def self.move(l : Int32, c : Int32)
+    print "\33[#{l+1};#{c+1}H"
+  end
 end
